2c42fc6017525dfd4531b08bf457d7d534ede4e8
sunchobe <christian@ober.info>
2014-04-30 20:42:46 +0200

    - added spring dependencies - basic test -> currently doing nothing

--- src/main/java/org/silverbullit/auditlog/AuditionTransactionHandler.java
+++ src/main/java/org/silverbullit/auditlog/AuditionTransactionHandler.java
@@ -2,4 +2,9 @@ package org.silverbullit.auditlog;
 
 public class AuditionTransactionHandler {
 
+	private static final ThreadLocal<AuditionTransactionContext> auditionTransactionContext = new ThreadLocal<>();
+	
+	public static AuditionTransactionContext getContext() {
+		return auditionTransactionContext.get();
+	}
 }
